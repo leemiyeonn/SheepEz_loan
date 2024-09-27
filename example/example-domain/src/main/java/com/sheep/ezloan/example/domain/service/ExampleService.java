d53e561a6c17b717d2517c9a6872664f9882ddb1
road3144 <fnfnfn258@gmail.com>
2024-09-27 12:19:18 +0900

    feat: PR Test

--- example/example-domain/src/main/java/com/sheep/ezloan/example/domain/service/ExampleService.java
+++ example/example-domain/src/main/java/com/sheep/ezloan/example/domain/service/ExampleService.java
@@ -2,7 +2,19 @@ package com.sheep.ezloan.example.domain.service;
 
 import org.springframework.stereotype.Service;
 
+import com.sheep.ezloan.example.domain.model.Example;
+import com.sheep.ezloan.example.domain.repository.ExampleRepository;
+
+import lombok.RequiredArgsConstructor;
+
 @Service
+@RequiredArgsConstructor
 public class ExampleService {
+
+    private final ExampleRepository exampleRepository;
+
+    public Example createExample(Example example) {
+        return exampleRepository.save(example);
+    }
 
 }
--- example/example-storage/src/main/java/com/sheep/ezloan/example/storage/implement/ExampleRepositoryAdapter.java
+++ example/example-storage/src/main/java/com/sheep/ezloan/example/storage/implement/ExampleRepositoryAdapter.java
@@ -1,7 +1,27 @@
 package com.sheep.ezloan.example.storage.implement;
 
-import com.sheep.ezloan.example.domain.repository.ExampleRepository;
+import java.util.UUID;
 
+import org.springframework.stereotype.Repository;
+
+import com.sheep.ezloan.example.domain.model.Example;
+import com.sheep.ezloan.example.domain.repository.ExampleRepository;
+import com.sheep.ezloan.example.storage.entity.ExampleEntity;
+
+import lombok.RequiredArgsConstructor;
+
+@Repository
+@RequiredArgsConstructor
 public class ExampleRepositoryAdapter implements ExampleRepository {
+
+    private final ExampleJpaRepository exampleJpaRepository;
+
+    @Override
+    public Example save(Example example) {
+        ExampleEntity entity = new ExampleEntity(UUID.randomUUID());
+        exampleJpaRepository.save(entity);
+
+        return null;
+    }
 
 }
