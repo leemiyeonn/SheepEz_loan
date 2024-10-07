dca2f4ce8f20a1df499356ae64d3d9a41d5682cf
leemiyeon <lemingul7@gmail.com>
2024-10-08 02:20:59 +0900

    ✨ feat: auth security 로직 구현 (#17)

---
+++ support/support-auth/src/main/java/com/sheep/ezloan/auth/utils/SecurityUtil.java
@@ -0,0 +1,15 @@
+package com.sheep.ezloan.auth.utils;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+public class SecurityUtil {
+    // 현재 인증된 사용자의 userId를 반환하는 메서드
+    public static Long getCurrentUserId() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication != null && authentication.getPrincipal() instanceof String) {
+            return Long.parseLong((String) authentication.getPrincipal()); // userId 반환
+        }
+        return null;
+    }
+}
